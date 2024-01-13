47744f9370745862fe6cf1ee7ec1510d6b36a682
Lee Canning <2116294@student.uwtsd.ac.uk>
2024-01-13 11:20:09 +0000

    Added a simple request to fetch json data from an api

--- NetProgAssignment2.py
+++ NetProgAssignment2.py
@@ -1,3 +1,10 @@
 import json
+import requests
 
 print("Hola Amigo")
+
+# simple request to fetch and display data from an api #
+url = "https://api.openbrewerydb.org/v1/breweries/random"
+response = requests.get(url)
+data = response.json()
+print(data)
