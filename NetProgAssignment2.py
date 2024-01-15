a2f39ebeef9c1ff9224d61626a79c536cd6c9923
Lee Canning <2116294@student.uwtsd.ac.uk>
2024-01-15 18:16:03 +0000

    The method random_brew() returns info from the API and has an exception check added.

--- NetProgAssignment2.py
+++ NetProgAssignment2.py
@@ -1,10 +1,23 @@
 import json
 import requests
 
-print("Hola Amigo")
 
-# simple request to fetch and display data from an api #
-url = "https://api.openbrewerydb.org/v1/breweries/random"
-response = requests.get(url)
-data = response.json()
-print(data)
+def random_brew():
+
+    try:
+
+        # simple request to fetch and display data from an api #
+        url = "https://api.openbrewerydb.org/v1/breweries/randoms"
+        response = requests.get(url)
+        print("http status code: ",  response.status_code, "\n")
+        response.raise_for_status()
+        # parses JSON string and converts into a Python dictionary
+        # json_data = json.loads(response.text)
+        json_data = response.json()
+        print(json_data)
+
+    except requests.exceptions.RequestException as e:
+        print("Error:", {e})
+
+
+random_brew()
