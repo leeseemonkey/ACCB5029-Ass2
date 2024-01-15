83e3df76455f27d69e09b8d6964684490d806d0f
Lee Canning <2116294@student.uwtsd.ac.uk>
2024-01-15 19:10:01 +0000

    Different methods of displaying the data - the one I want is the for loop version as it's nice and clean. That's the basics of retrieving the data, so now I just need to review the functionality I want the program to have.

--- NetProgAssignment2.py
+++ NetProgAssignment2.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import pprint
 
 
 def random_brew():
@@ -7,14 +8,24 @@ def random_brew():
     try:
 
         # simple request to fetch and display data from an api #
-        url = "https://api.openbrewerydb.org/v1/breweries/randoms"
+        url = "https://api.openbrewerydb.org/v1/breweries/random"
         response = requests.get(url)
         print("http status code: ",  response.status_code, "\n")
         response.raise_for_status()
         # parses JSON string and converts into a Python dictionary
         # json_data = json.loads(response.text)
         json_data = response.json()
-        print(json_data)
+        print("Unformatted JSON data:", json_data, "\n")
+
+        print("As a Python dictionary", json_data[0], "\n")
+
+        print("Using pprint function \n")
+        pprint.pprint(json_data)
+        print("-----done--------")
+
+        # iterates through the dictionary and prints key value pairs
+        for key, value in json_data[0].items():
+            print(key, ": ", value)
 
     except requests.exceptions.RequestException as e:
         print("Error:", {e})
