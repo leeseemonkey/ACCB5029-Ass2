e70875668255a84d99341a3633e89214daf260b9
Lee Canning <2116294@student.uwtsd.ac.uk>
2024-01-16 20:15:21 +0000

    Code for searching brewery works - now to adapt to add parameters from user input

--- NetProgAssignment2.py
+++ NetProgAssignment2.py
@@ -1,35 +1,57 @@
 import json
 import requests
-import pprint
 
 
-def random_brew():  #Displays a random brewery
+def random_brew():  # Displays a random brewery
 
     try:
 
         # simple request to fetch and display data from an api #
-        url = "https://api.openbrewerydb.org/v1/breweries/random"
+        url = "https://api.openbrewerydb.org/v1/breweries?by_city=miami&per_page=3"
         response = requests.get(url)
-        print("http status code: ",  response.status_code, "\n")
-        response.raise_for_status()
-        # parses JSON string and converts into a Python dictionary
-        json_data = json.loads(response.text)
+        print("http status code: ",  response.status_code, "- ", response.reason)
+        # response.raise_for_status()
 
         # json_data = response.json()
-        print("Unformatted JSON data:", json_data, "\n")
+        # parses JSON string and converts into a Python dictionary
+        # but looks like json is an array so parses it into a list :(
+        json_data = json.loads(response.text)
+        print(json_data)
+        print("Type: ", type(json_data), "\n")
+       #  print("The name should be here: ", json_data['name'])
+       #  print("City: ", json_data['city'])
 
-        print("As a Python dictionary", json_data[0], "\n")
+        print("JSON array as Python list: ", json_data[0])
 
-        print("Using pprint function \n")
-        pprint.pprint(json_data)
-        print("-----done--------")
+        print("Trying iter: ", iter(json_data))
+
+        # json_dict = {}
+        # for element in json_data:
+        #    json_dict[element] = element
+        # print("Dict converter: ", json_dict, "\n")
+
+
+        # json_data = response.json() # Returns a JSON object of the result (if the result was written in JSON format
+        # json_dict = json_data[0]
+        # print(json_dict)
+
+        for index, item in enumerate(json_data):
+            print("New for loop: ", item)
+        print(json_data['name'])
 
         # iterates through the dictionary and prints key value pairs
-        for key, value in json_data[0].items():
-            print(key, ": ", value)
+        # though this one doesn't iterate, it just looks at the first item.
 
+        # for key, value in enumerate(json_data.items()):
+        #    print(key, ": ", value, "\n---------------")
+
+        # for key, value in json_data.items():
+        #    print("{} : {} ".format(key, value))
+
+    # is there a better way of printing this error?
     except requests.exceptions.RequestException as e:
         print("Error:", {e})
+        print(type(e))
 
 
 random_brew()
